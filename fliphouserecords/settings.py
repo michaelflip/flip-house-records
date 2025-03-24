38d56c51d8368443703747b067f77bd25fe7544a
Michael Lewis <michaeladamlewis@gmail.com>
2025-03-23 23:56:33 -0700

    Fix STATIC_URL leading slash for image loading

--- fliphouserecords/settings.py
+++ fliphouserecords/settings.py
@@ -14,7 +14,7 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 # Static file settings
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
