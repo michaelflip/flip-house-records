0dbd3564dc5d177aa26c79b7734c74b33e18a41c
Michael Lewis <michaeladamlewis@gmail.com>
2025-03-23 23:12:59 -0700

    Fix tag filter URL name for release_list_by_tag

--- releases/urls.py
+++ releases/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.release_list, name='release_list'),
-    path('tag/<str:tag>/', views.release_list, name='release_tag_filter'),
+    path('tag/<str:tag>/', views.release_list, name='release_list_by_tag'), 
 ]
