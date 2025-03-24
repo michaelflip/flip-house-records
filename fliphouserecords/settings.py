91b09d946c970e7b38f7eddcae4dc18550334f8b
Michael Lewis <michaeladamlewis@gmail.com>
2025-03-24 00:10:29 -0700

    Fix static files and storage config

--- fliphouserecords/settings.py
+++ fliphouserecords/settings.py
@@ -15,9 +15,10 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 # Static file settings
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
