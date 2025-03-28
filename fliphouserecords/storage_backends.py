5eeb1f947a15b27f3ff124b2b9ceea44bfbbb6fd
Michael Lewis <michaeladamlewis@gmail.com>
2025-03-28 01:50:43 -0700

    added more to storage backends

--- fliphouserecords/storage_backends.py
+++ fliphouserecords/storage_backends.py
@@ -1,17 +1,20 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
-class PublicMediaStorage(S3Boto3Storage):
-    location = ''  # leave empty so files aren't put in a 'media/' subfolder unless you want that
-    file_overwrite = False  # keeps the same filename, doesn't rename
+class StaticStorage(S3Boto3Storage):
+    location = 'static'
     default_acl = 'public-read'
-
-    def get_available_name(self, name, max_length=None):
-        # Prevent Django from renaming the file if it exists — instead overwrite
-        if self.exists(name):
-            self.delete(name)
-        return name
 
 class MediaStorage(S3Boto3Storage):
     location = ''
     file_overwrite = False
     default_acl = 'public-read'
+
+class PublicMediaStorage(S3Boto3Storage):
+    location = ''
+    file_overwrite = False
+    default_acl = 'public-read'
+
+    def get_available_name(self, name, max_length=None):
+        if self.exists(name):
+            self.delete(name)
+        return name
