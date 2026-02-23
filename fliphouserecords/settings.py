f71b0a5c7a99501e70ccfee90d692cd1cb8ac508
Michael Lewis <michaelflip@gmail.com>
2026-02-22 19:01:44 -0800

    update settings.py
    update settings

--- fliphouserecords/settings.py
+++ fliphouserecords/settings.py
@@ -113,6 +113,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # === Production Security ===
 if not DEBUG:
     SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')  # ADD THIS LINE
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
 
