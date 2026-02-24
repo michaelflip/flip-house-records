2084f214b9b6dbd39150740fc80a1fe49bab8500
michaelflip <michaelflip@gmail.com>
2026-02-23 21:00:34 -0800

    update urls.py
    update urls.py

--- fliphouserecords/urls.py
+++ fliphouserecords/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from releases.views import homepage, artists, contact, merch, events, links, wall, password_reset_confirm
+from releases.views import homepage, artists, contact, merch, events, links, wall, password_reset_confirm, update_profile, get_profile
 
 urlpatterns = [
     path('', homepage, name='home'),
@@ -15,6 +15,10 @@ urlpatterns = [
     path('links/', links, name='links'),
     path('wall/', wall, name='wall'),
     path('wall/reset-password/<str:token>/', password_reset_confirm, name='password_reset_confirm'),
+    
+    # REMOVED the 'views.' prefix since we imported them directly above
+    path('api/profile/update/', update_profile, name='update_profile'),
+    path('api/profile/<str:username>/', get_profile, name='get_profile'),
 ]
 
 if settings.DEBUG:
